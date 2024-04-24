d7f010e5b37db264358266456955079b20aba533
John W. <112655410+jozhw@users.noreply.github.com>
2024-04-24 13:35:29 -0500

    update: add next run

--- src/sig/get_error.py
+++ src/sig/get_error.py
@@ -21,6 +21,7 @@ DATA = [
     "results/synthetic/2024-04-18/synthetic_imgs_adjusted_entropy_and_mean_results.csv",
     "results/synthetic/2024-04-19/synthetic_imgs_results.csv",
     "results/synthetic/2024-04-19/synthetic_imgs_adjusted_entropy_and_mean_results.csv",
+    "results/synthetic/2024-04-24/synthetic_imgs_results.csv",
 ]
 
 
